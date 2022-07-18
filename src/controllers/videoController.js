040021abd910b0a9108b3ac8a3c09cbc152fab31
yoojinLiz <yj.oh.biz@gmail.com>
2022-07-19 03:05:40 +0900

    header padding value updated

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ import Comment from "../models/Comment"
 
 export const trending = async(req,res)=> {
 	const videos = await Video.find({}).sort({createdAt:"desc"}).populate("owner");
-	return res.render("home", {pageTitle:"Home", videos});
+    return res.render("home", {pageTitle:"Home", videos});
 };
 
 export const watch = async(req,res) => {
